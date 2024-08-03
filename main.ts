ab3aabec381fdd6143e8ce54d58d687f411271f2
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-08-03 14:55:35 -0500

    Initialize typing game in TypeScript with score and streaks

---
+++ main.ts
@@ -0,0 +1,66 @@
+let score: number
+let streak: number
+let time: number
+let isGameStarted = false;
+let isGameEnded = true;
+let character: string
+let characterText = document.getElementById('character-text') as HTMLSpanElement;
+let scoreText = document.getElementById('score') as HTMLSpanElement;
+let timeText = document.getElementById('time') as HTMLSpanElement;
+let typingText = document.getElementById('typing-text') as HTMLInputElement;
+let startButton = document.getElementById('start-button') as HTMLButtonElement;
+function startGame() {
+    score = 0;
+    streak = 0;
+    time = 60;
+    isGameStarted = true;
+    isGameEnded = false;
+    generateRandomCharacter();
+    scoreText.textContent = score.toString();
+    timeText.textContent = time.toString();
+    typingText.focus();
+    startButton.disabled = true;
+    const updateTime = () => {
+        time--;
+        timeText.textContent = time.toString();
+        if (time === 0) {
+            clearInterval(updateTimeInterval);
+            isGameStarted = false;
+            isGameEnded = true;
+            startButton.disabled = false;
+        }
+    }
+    const updateTimeInterval = setInterval(updateTime, 1000);
+}
+function generateRandomCharacter() {
+    const lowercaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const symbols = '`~!@#$%^&*()_+-={}|[]\\:";\'><?,./';
+    const alphabet = lowercaseAlphabet + uppercaseAlphabet;
+    const lettersAndNumbers = alphabet + numbers;
+    const charactersList = lettersAndNumbers + symbols;
+    character = alphabet[Math.floor(Math.random() * alphabet.length)];
+    characterText.textContent = character;
+}
+startButton.addEventListener('click', () => {
+    if (!isGameStarted || isGameEnded) {
+        startGame();
+        startButton.disabled = true;
+    }
+});
+typingText.addEventListener('input', () => {
+    if (isGameStarted && !isGameEnded) {
+        const text: string = typingText.value;
+        if (text === character) {
+            streak++;
+            score += streak;
+            typingText.value = '';
+            scoreText.textContent = score.toString();
+            generateRandomCharacter();
+        }
+        else {
+            streak = 0;
+        }
+    }
+});
